e560b1d5e4f84fd8fabbdc171174c7087e0e011e
Yasas Banuka <yixbinfo@gmail.com>
2025-11-16 20:07:48 +0530

    feat(movie-card): completed movie card to display proper info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -69,9 +70,7 @@ function App() {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p key={movie.id} className="text-white">
-                  {movie.title}
-                </p>
+                  <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
