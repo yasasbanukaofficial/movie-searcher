cfce883103f011cccb6cedbeeda788dea9ea9e84
Yasas Banuka <yixbinfo@gmail.com>
2025-11-16 19:35:39 +0530

    feat(main): implemented spinner, displays movie titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -60,8 +61,20 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
         <section className="all-movies">
-          <h2>All Movies</h2>
-          {errorMsg && <p className="text-red-500">{errorMsg}</p>}
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMsg ? (
+            <p className="text-red-500">Error when loading movie list</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
         </section>
       </div>
     </main>
